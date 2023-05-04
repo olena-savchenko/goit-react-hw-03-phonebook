c04d73f5d27e079e0d79756accccb4cef5eb8dbf
olena-savchenko <112640422+olena-savchenko@users.noreply.github.com>
2023-05-04 13:37:46 +0300

    update src

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,80 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { nanoid } from 'nanoid';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { Layout } from './Layout/Layout';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  handleInputChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  addContact = ({ name, number }) => {
+    const hasName = this.state.contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (hasName) {
+      return alert(`${name} is already in contacts`);
+    }
+
+    this.setState(prevState => {
+      return {
+        contacts: [
+          ...prevState.contacts,
+          {
+            id: nanoid(),
+            name: name,
+            number: number,
+          },
+        ],
+      };
+    });
+  };
+
+  filter = () => {
+    const { contacts, filter } = this.state;
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: this.state.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    return (
+      <Layout>
+        <h1>Phonebook</h1>
+        <ContactForm addContact={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter
+          filter={this.state.filter}
+          handleFilter={this.handleInputChange}
+        />
+        <ContactList
+          contacts={this.filter()}
+          deleteContact={this.deleteContact}
+        />
+      </Layout>
+    );
+  }
+}
